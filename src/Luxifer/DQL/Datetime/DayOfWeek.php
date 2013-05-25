e5b7ec757487de41c2d809cb817f9180c2983567
luxifer <luxifer666@gmail.com>
2013-05-25 14:09:32 +0200

    add: hour function add: dayofweek function add: Numeric and String folder

---
+++ src/Luxifer/DQL/Datetime/DayOfWeek.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Luxifer\DQL\Datetime;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * DateFunction ::= "DAYOFWEEK" "(" ArithmeticPrimary ")"
+ * @author Florent Viel <fviel@wanadev.fr>
+ */
+class DayOfWeek extends FunctionNode
+{
+    public $dateExpression;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'DAYOFWEEK(' .
+            $this->dateExpression->dispatch($sqlWalker) .
+        ')'; 
+    }
+}
---
+++ src/Luxifer/DQL/Datetime/Hour.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Luxifer\DQL\Datetime;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * DateFunction ::= "HOUR" "(" ArithmeticPrimary ")"
+ * @author Florent Viel <fviel@wanadev.fr>
+ */
+class Hour extends FunctionNode
+{
+    public $dateExpression;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'HOUR(' .
+            $this->dateExpression->dispatch($sqlWalker) .
+        ')'; 
+    }
+}
