ddde7822f93ad206425edfa20862e9efe615eefd
luxifer <luxifer666@gmail.com>
2013-05-25 14:26:04 +0200

    add: dayofmonth, dayofyear, minute, month, second, time and year function

---
+++ src/Luxifer/DQL/Datetime/DayOfMonth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Luxifer\DQL\Datetime;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * DateFunction ::= "DAYOFMONTH" "(" ArithmeticPrimary ")"
+ * @author Florent Viel <fviel@wanadev.fr>
+ */
+class DayOfMonth extends FunctionNode
+{
+    public $dateExpression;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'DAYOFMONTH(' .
+            $this->dateExpression->dispatch($sqlWalker) .
+        ')'; 
+    }
+}
