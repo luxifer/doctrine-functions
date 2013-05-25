97f9a0dd4770ba4b71a5698688742cc0a275b910
luxifer <luxifer666@gmail.com>
2013-05-25 13:49:08 +0200

    add: mysql DATE() function

---
+++ src/Luxifer/DQL/Date.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Luxifer\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * DateFunction ::= "DATE" "(" ArithmeticPrimary ")"
+ * @author Florent Viel <fviel@wanadev.fr>
+ */
+class Date extends FunctionNode
+{
+    public $dateExpression;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'DATE(' .
+            $this->dateExpression->dispatch($sqlWalker) .
+        ')'; 
+    }
+}
