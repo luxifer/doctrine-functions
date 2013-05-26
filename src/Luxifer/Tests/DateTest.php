4961e402a326d1cfc36f47bef45a6c1598cab143
luxifer <luxifer666@gmail.com>
2013-05-26 08:36:30 +0200

    edit: bootstrap test

--- src/Luxifer/Tests/DateTest.php
+++ src/Luxifer/Tests/DateTest.php
@@ -7,8 +7,8 @@ class DateTest extends DQLFunctionTest
     public function testDate()
     {
         $em = $this->_getEntityManager();
-        $query = $em->createQuery("SELECT DATE('2003-12-31 01:02:03')");
+        $query = $em->createQuery("SELECT DATE('2003-12-31 01:02:03') FROM ");
 
-        $this->assertEquals($query->getSQL(), "SELECT DATE('2003-12-31 01:02:03')");
+        $this->assertEquals($query->getSQL(), "SELECT DATE('2003-12-31 01:02:03') FROM ");
     }
 }
