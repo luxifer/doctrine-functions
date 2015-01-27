1226fa165b8d3bc3d687e241a9aebc91c272a332
Antonio Peric <antonio@locastic.com>
2015-01-27 23:51:18 +0100

    Added rand function

---
+++ src/Luxifer/DQL/MIsc/Rand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Luxifer\DQL\Misc;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class RandFunction extends FunctionNode
+{
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'RAND()';
+    }
+}
