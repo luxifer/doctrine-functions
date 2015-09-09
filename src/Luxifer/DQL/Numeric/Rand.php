6889424af40b6688da280817c9a11aba56145f86
Florent Viel <florent.viel69@gmail.com>
2015-09-09 12:04:51 +0200

    fix rand

--- src/Luxifer/DQL/Numeric/Rand.php
+++ src/Luxifer/DQL/Numeric/Rand.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 
-class RandFunction extends FunctionNode
+class Rand extends FunctionNode
 {
     public function parse(Parser $parser)
     {
