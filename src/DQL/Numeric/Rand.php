8aa472dc97746e3facdebfa94eb0461816859964
Aleksandr Kalugin <gregsparroww@gmail.com>
2025-02-11 16:57:58 +0200

    Update Rand.php

--- src/DQL/Numeric/Rand.php
+++ src/DQL/Numeric/Rand.php
@@ -9,14 +9,14 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 
 class Rand extends FunctionNode
 {
-    public function parse(Parser $parser)
+    public function parse(Parser $parser): void
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
-    public function getSql(SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
         return 'RAND()';
     }
